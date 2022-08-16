e6beafac738624ecc355613b5bcb80a423615ed9
maldoem <emilianomaldonez2@gmail.com>
2022-08-16 00:05:32 -0300

    desafio clase 6

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function login() {
     
     let ingresar = Number(prompt("ingrese su nombre de usuario y contraseña o registrese \n1. Tengo usuario \n2. No tengo usuario"))
 
-    function crearUsuario() {
+    function crearUsuario(){
         let nuevoUsuario = prompt("ingrese su nombre de usuario")
         let nuevaContraseña = prompt("ingrese su nueva clave")
         listaUsuarios.push({usuario : nuevoUsuario, contraseña: nuevaContraseña})
@@ -44,18 +44,37 @@ function carrito(){
     const art3 = new remeras("azul", "L", 1200)
     
     let pedido = Number(prompt(" seleccione que articulo desea comprar \n1 art1. remera gris talle S\n2 art2.remera negra talle  M\n3 art3.remera azul talle L"))
-    if( pedido == 1 ){
-        alert( " su pedido de remera " + art1.color + " es de un valor de " + art1.precio)
-    } if( pedido == 2 ){
-        alert( " su pedido de remera " + art2.color + " es de un valor de " + art2.precio) 
-    } if( pedido == 3 ){
-        alert( " su pedido de remera " + art3.color + " es de un valor de " + art3.precio)
-        }if(pedido != 1 || 2 || 3){
-            pedidoErroneo = Number(prompt(" quiere realizar su pedido correctamente \n.1 si \n.2 no "))
-        }if (pedidoErroneo == 2){
-            alert("no vuelva mas")
-        }else if (pedidoErroneo == 1){
-            alert("vuelva a realizar el pedido")
-            carrito()
+
+    function error(){
+            let pedidoErroneo = (prompt(" quiere realizar su pedido correctamente \n. si \n. no "))
+            if (pedidoErroneo == "si"){
+                carrito()
+            }
+            else if (pedidoErroneo == "no"){    
+                alert("no vuelva mas")
         }
-}
+    }
+        if( pedido == 1 ){
+            alert( " su pedido de remera " + art1.color + " es de un valor de " + art1.precio)
+            alert( " gracias por su compra ")
+        }
+         else if( pedido == 2 ){
+            alert( " su pedido de remera " + art2.color + " es de un valor de " + art2.precio)
+            alert( " gracias por su compra ")
+        }
+        else if( pedido == 3 ){
+            alert( " su pedido de remera " + art3.color + " es de un valor de " + art3.precio)
+            alert( " gracias por su compra ")
+            }else{
+                error()
+            }
+}            
+
+            
+                           
+         
+        
+
+   
+        
+
