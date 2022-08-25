1f7cc1af59e27bd7c79323bc2739194a8c67a950
maldoem <emilianomaldonez2@gmail.com>
2022-08-24 21:22:00 -0300

    desafio clase 9

--- app.js
+++ app.js
@@ -1,80 +1,19 @@
+let comprar = document.getElementById('comprar')
 
-function login() {
-    const usuar1 = { usuario : "emiliano88", contraseña: "clave1"}
-    const usuar2 = { usuario : "ezequiel90", contraseña: "clave2"}
-    const usuar3 = { usuario : "camila95", contraseña: "clave3"}
+function seleccionColor(){
+    let color = document.getElementById('color').value
+    let talle = document.getElementById('talle').value
+    let small = document.getElementById('small').value
+    let medium = document.getElementById('medium').value
+    let large = document.getElementById('large').value
 
-    const listaUsuarios = [usuar1, usuar2, usuar3]
+    if(talle == small){
+        document.getElementById('total').innerHTML = "Usted selecciono la remera " + color + " de talle " + talle + " y Un valor de $1000"
+    }else if (talle == medium){
+        document.getElementById('total').innerHTML = "Usted selecciono la remera " + color + " de talle " + talle + " y un valor de $1100"
+    }else if(talle == large){
+        document.getElementById('total').innerHTML = "Usted selecciono la remera " + color + " de talle " + talle + " y un valor de $1200"
+    }
     
-    let ingresar = Number(prompt("Ingrese su nombre de usuario y contraseña o registrese \n1. Tengo usuario \n2. No tengo usuario"))
-
-    function crearUsuario(){
-        let nuevoUsuario = prompt("Ingrese su nombre de usuario")
-        let nuevaContraseña = prompt("Ingrese su nueva clave")
-        listaUsuarios.push({usuario : nuevoUsuario, contraseña: nuevaContraseña})
-        alert("Creaste tu nuevo usuario bajo el nombre de " + nuevoUsuario)
-        carrito()
-    }  
-        if(ingresar == 1){
-        let usuarioExistente = prompt("Ingrese su nombre de usuario")
-        let contraseñaExistente = prompt("Ingrese su contraseña")
-        for (const datos of listaUsuarios){
-    
-        if(datos.usuario == usuarioExistente && datos.contraseña == contraseñaExistente){
-            alert("Bienvenido " + usuarioExistente)
-            carrito() 
-        }else{
-            alert("Datos incorrectos, vuelva a ingresarlos")
-            break
-        }}}        
-        else if(ingresar == 2){
-        crearUsuario()
-        }           
 }
-login()
-
-function carrito(){
-    function remeras(color, talle, precio){
-    this.color = color;
-    this.talle = talle;
-    this.precio = Number(precio); 
-    }
-    const art1 = new remeras("gris", "S", 1000)
-    const art2 = new remeras("negra", "M", 1100)
-    const art3 = new remeras("azul", "L", 1200)
-    
-    let pedido = Number(prompt(" Seleccione que articulo desea comprar \n1 art1. remera gris talle S\n2 art2.remera negra talle  M\n3 art3.remera azul talle L"))
-
-    function error(){
-            let pedidoErroneo = (prompt(" Quiere realizar su pedido correctamente \n. si \n. no "))
-            if (pedidoErroneo == "si"){
-                carrito()
-            }
-            else if (pedidoErroneo == "no"){    
-                alert("No vuelva mas")
-        }
-    }
-        if( pedido == 1 ){
-            alert( " Su pedido de remera " + art1.color + " es de un valor de " + art1.precio)
-            alert( " Gracias por su compra ")
-        }
-         else if( pedido == 2 ){
-            alert( " Su pedido de remera " + art2.color + " es de un valor de " + art2.precio)
-            alert( " Gracias por su compra ")
-        }
-        else if( pedido == 3 ){
-            alert( " Su pedido de remera " + art3.color + " es de un valor de " + art3.precio)
-            alert( " Gracias por su compra ")
-            }else{
-                error()
-            }
-}            
-
-            
-                           
-         
-        
-
-   
-        
-
+comprar.addEventListener('click', seleccionColor)
